e60de4a17651a72f6de607fa0ab7ddd8cc707aa7
ankita19t <ankitat19799@gmail.com>
2021-12-25 12:35:47 +0530

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,7 @@
+-- update email address of user with id = 2
+
+UPDATE mytable SET email = 'ankita@22.com' WHERE id = 2;
+
+-- update multiple columns of user with id = 2
+
+UPDATE mytable SET email = 'ankita@22.com', name = 'Avinash Seth' WHERE id = 2;
